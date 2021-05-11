996d611511eb3a1fae607a831510fbf59a614f99
Patricia <aaraujo.patricia@gmail.com>
2021-05-11 15:45:20 -0300

    Segundo join

--- pizza_joins2.sql
+++ pizza_joins2.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE db_pizzaria_legal;
 
 CREATE TABLE tb_pizza (
-    id_pizza INT NOT NULL ,
+    id_pizza INT NOT NULL AUTO_INCREMENT ,
     nome VARCHAR (20) NOT NULL,
     preco DOUBLE NOT NULL,
     tamanho VARCHAR (15),
@@ -9,7 +9,8 @@ CREATE TABLE tb_pizza (
     categoria VARCHAR (20),
     fk_categoria INT NOT NULL,
     PRIMARY KEY (id_pizza),
-     FOREIGN KEY (`fk_categoria`)
+	CONSTRAINT `fk_tb_pizza_tb_categoria`
+    FOREIGN KEY (`fk_categoria`)
     REFERENCES `db_pizzaria_legal`.`tb_categoria` (`id_categoria`)
 );
 
@@ -21,13 +22,26 @@ CREATE TABLE tb_categoria (
     PRIMARY KEY (id_categoria)
 
 );
-INSERT INTO tb_categoria (nome, doce, salgada)
-VALUES ("casadinho", "goiabada", "queijo"),
-("ImprovavelPoremBom", "banana", "queijo"),
-("BomDeMais"," brigadeiro", "queijo");
+INSERT INTO tb_categoria (nome, sabor, promocao)
+VALUES ("Frango e queijo", "salgada", true),
+("Carne seca e queijo", "salgada", true),
+("Banana com canela"," doce", true);
 
-ALTER TABLE tb_pizza
-MODIFY COLUMN id_pizza INT NOT NULL AUTO_INCREMENT;
+SELECT * FROM tb_categoria;
 
-ALTER TABLE tb_categoria
-MODIFY COLUMN id_categoria INT NOT NULL AUTO_INCREMENT;
+SELECT * FROM tb_pizza;
+
+INSERT INTO tb_pizza (nome, preco, tamanho, disponibilidade, categoria, fk_categoria)
+VALUES ( "Fangro Padrão", 20,"grande", true, "frango e queijo", 1),
+("ImprovavelPoremBom",36, "media", false, "Carne seca e queijo", 2 ),
+("Sobremesa", 15, "pequena",true, "Banana com calena", 3);
+
+SELECT * FROM db_pizzaria_legal.tb_pizza WHERE preco > 45;
+
+SELECT * FROM db_pizzaria_legal.tb_pizza WHERE preco  BETWEEN 29 AND 60;
+
+SELECT * FROM db_pizzaria_legal.tb_pizza WHERE nome LIKE "%c%";
+
+SELECT * FROM db_pizzaria_legal.tb_pizza INNER JOIN db_pizzaria_legal.tb_categoria ON tb_categoria.id_categoria = tb_pizza.fk_categoria;
+
+
